311637600aee74a62f04c635b13614f705aebac6
Vincent Blzt <vincentbalazutpro@gmail.com>
2026-09-24 16:03:30 +0200

    Delete Clean renov (code) directory

--- Clean renov (code)/src/App.jsx
+++
@@ -1,5 +0,0 @@
-import CleanRenov38Landing from "./CleanRenov38Landing";
-
-export default function App() {
-  return <CleanRenov38Landing />;
-}
--- Clean renov (code)/src/data/avantApres.js
+++
@@ -1,9 +0,0 @@
-import avant1 from "../assets/avant-apres/avant-1.jpeg";
-import avant2 from "../assets/avant-apres/avant-2.jpeg";
-import apres1 from "../assets/avant-apres/apres-1.jpeg";
-import apres2 from "../assets/avant-apres/apres-2.jpeg";
-
-export const avantApres = [
-  { id: 1, before: avant1, after: apres1 },
-  { id: 2, before: avant2, after: apres2 },
-];
